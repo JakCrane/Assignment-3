8f8247cfa636e5ce90a90e4840fc3de1aefaa117
JakCrane <jackx.crane@hotmail.com>
2023-03-20 11:35:17 +0000

    Committing pre-html inheritance

---
+++ cafcafproj/custapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'custapp/index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'custapp/about.html')
